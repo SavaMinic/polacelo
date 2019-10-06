088fdc24ad3cdb714012ebd8256928e286deb05d
Sava Minic <savam@nordeus.com>
2019-10-06 17:02:29 +0200

    base for scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Definition
+
+    public enum GameState
+    {
+        MainMenu,
+        Playing,
+        EndGame,
+    }
+
+    #endregion
+
+    #region Singleton
+
+    public static GameManager I { get; private set; }
+
+    #endregion
+
+    #region Actions
+
+    public Action OnGameStateChanged;
+
+    #endregion
+
+    #region Fields
+
+    private GameState state;
+
+    #endregion
+
+    #region Properties
+
+    public GameState State
+    {
+        get => state;
+        set
+        {
+            state = value;
+            OnGameStateChanged?.Invoke();
+        }
+    }
+
+    #endregion
+
+    #region Mono
+
+    private void Awake()
+    {
+        I = this;
+    }
+
+    private void Start()
+    {
+        State = GameState.MainMenu;
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/UI/CanvasController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    #region Singleton
+
+    public static CanvasController I { get; private set; }
+
+    #endregion
+
+    #region Fields
+
+    [SerializeField]
+    private CanvasGroup mainMenuGroup;
+
+    [SerializeField]
+    private Button playButton;
+
+    #endregion
+
+    #region Mono
+
+    private void Awake()
+    {
+        I = this;
+    }
+
+    #endregion
+}
