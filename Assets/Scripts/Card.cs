d0111c9d57cb088174a6423e7cb2e727fc46e10c
Sava Minic <savam@nordeus.com>
2019-10-06 18:40:04 +0200

    card material, physics and test camera

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private Rigidbody myRigidbody;
+    
+    void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    #region Public
+
+    public void ResetTo(Vector3 pos, Quaternion? rot = null)
+    {
+        transform.position = pos;
+        transform.rotation = rot ?? Quaternion.identity;
+        
+        myRigidbody.velocity = Vector3.zero;
+        myRigidbody.angularVelocity = Vector3.zero;
+    }
+
+    #endregion
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
@@ -30,6 +32,9 @@ public class GameManager : MonoBehaviour
 
     #region Fields
 
+    [SerializeField]
+    private Card firstCard;
+
     private GameState state;
 
     #endregion
@@ -57,7 +62,39 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (!Application.isPlaying)
+            return;
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            NewGame();
+        }
+    }
+
+    #endregion
+
+    #region Private
+
+    public void NewGame()
+    {
         State = GameState.Playing;
+        
+        // reset cards
+        firstCard.ResetTo(new Vector3(
+            Random.Range(-2f, 2f),
+            Random.Range(17f, 18f),
+            20f
+        ), Quaternion.Euler(
+            Random.Range(-45f, 45f),
+            Random.Range(-90f, 90f),
+            Random.Range(-45f, -45f)
+        ));
     }
 
     #endregion
